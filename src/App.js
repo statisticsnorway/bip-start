9fffaebe25107b040695422419341f9790896e66
Lisa Wold Eriksen <lwe@ssb.no>
2021-05-18 15:22:25 +0200

    Update test
    Internal ref [STRATUS-728]
    
    Co-authored-by: ArielKarlsen <54323769+ArielKarlsen@users.noreply.github.com>
    Co-authored-by: ssbwep <36296800+ssbwep@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -7,10 +7,10 @@ function App() {
         <p>
           Welcome to BIP-Start! Here are some useful links:
         </p>
-          <a className="App-link" href="https://statistics-norway.atlassian.net/wiki/spaces/BIP/overview">BIP wiki</a>
-          <a className="App-link" href="https://github.com/statisticsnorway/ssb-developer-guide">SSB developer guide</a>
-          <a className="App-link" href="https://github.com/statisticsnorway/platform-dev">SSB platform-dev</a>
-          <a className="App-link" href="https://statistics-norway.atlassian.net/browse/STRATUS">STRATUS jira site</a>
+        <a className="App-link" href="https://statistics-norway.atlassian.net/wiki/spaces/BIP/overview">BIP wiki</a>
+        <a className="App-link" href="https://github.com/statisticsnorway/ssb-developer-guide">SSB developer guide</a>
+        <a className="App-link" href="https://github.com/statisticsnorway/platform-dev">SSB platform-dev</a>
+        <a className="App-link" href="https://statistics-norway.atlassian.net/browse/STRATUS">STRATUS jira site</a>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('includes the four required links', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+
+  // Testing that the four links required are there. (Also requires the link text to be what we define here,
+  // which is not the intention, but these links will be removed and replaced by actual content later.)
+  expect(screen.getByText("BIP wiki").href).toBe("https://statistics-norway.atlassian.net/wiki/spaces/BIP/overview");
+  expect(screen.getByText("SSB developer guide").href).toBe("https://github.com/statisticsnorway/ssb-developer-guide");
+  expect(screen.getByText("SSB platform-dev").href).toBe("https://github.com/statisticsnorway/platform-dev");
+  expect(screen.getByText("STRATUS jira site").href).toBe("https://statistics-norway.atlassian.net/browse/STRATUS");
 });
