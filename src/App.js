d0ba2ab6a11a9f82efc31d8a26a5465dbb9b2202
Weihai Pan <wep@ssb.no>
2021-05-18 13:23:07 +0200

    Some preparations for React setup

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World!
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to BIP-Start! Here are some useful links:
         </p>
+          <a className="App-link" href="https://statistics-norway.atlassian.net/wiki/spaces/BIP/overview">BIP wiki</a>
+          <a className="App-link" href="https://github.com/statisticsnorway/ssb-developer-guide">SSB developer guide</a>
+          <a className="App-link" href="https://github.com/statisticsnorway/platform-dev">SSB platform-dev</a>
+          <a className="App-link" href="https://statistics-norway.atlassian.net/browse/STRATUS">STRATUS jira site</a>
       </header>
     </div>
   );
