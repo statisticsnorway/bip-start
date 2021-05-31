a65a1f980fabdb333dff55ce35b3f7d107d598d4
Lars-Roger Billingsø <lrb@ssb.no>
2021-05-31 14:36:23 +0200

    Removed links from first iteration

--- src/App.js
+++ src/App.js
@@ -7,12 +7,9 @@ function App () {
     <div className='App'>
       <header className='App-header'>
         <p>
-          Welcome to BIP-Start! Here are some useful links:
+          Welcome to BIP-Start!
         </p>
-        <a className='App-link' href='https://statistics-norway.atlassian.net/wiki/spaces/BIP/overview'>BIP wiki</a>
-        <a className='App-link' href='https://github.com/statisticsnorway/ssb-developer-guide'>SSB developer guide</a>
-        <a className='App-link' href='https://github.com/statisticsnorway/platform-dev'>SSB platform-dev</a>
-        <a className='App-link' href='https://statistics-norway.atlassian.net/browse/STRATUS'>STRATUS jira site</a>
+        
       </header>
       <HRForm/>
      </div>
